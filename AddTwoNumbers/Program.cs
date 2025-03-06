dbb86c91ddf9aab24f2c7a73db0c9423d79f91aa
CodeLock <105467967+C0d3L0ck@users.noreply.github.com>
2025-03-06 18:01:46 -0400

    Agregar archivos de proyecto.

---
+++ AddTwoNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿public class ListNode
+{
+      public int val;
+      public ListNode next;
+public ListNode(int val = 0, ListNode next = null)
+{
+        this.val = val;
+        this.next = next;
+}
+ }
+
+public class Solution
+{
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        l1.val += l2.val;
+        l2.next = l1;
+        return l1;
+    }
+}
